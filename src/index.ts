2ac533c52725c432042868dd2855614d767b037e
Alejandro Castillo <42857650+acast12@users.noreply.github.com>
2026-06-05 19:25:00 -0700

    fix port to use process.env.PORT for railway

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,5 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 app.use(router);
-app.listen(3001, () => console.log('API running on port 3001'));
+const port = process.env.PORT || 3001
+app.listen(port, () => console.log(`API running on port ${port}`));
